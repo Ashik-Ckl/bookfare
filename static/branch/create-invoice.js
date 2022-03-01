eecd41ed8e1aaf5c16546c99d680bd8c6dd20f45
Ubuntu <ashikchenekkal996@gmail.com>
2022-03-01 09:11:51 +0530

    new changes

--- static/branch/create-invoice.js
+++ static/branch/create-invoice.js
@@ -14,69 +14,33 @@ $("#invoiceForm").submit(function(){
         },
         statusCode: {
             200: function (response) {
+                console.log(data.length)
                 console.log(response[0]['id'])
                 console.log(response[0].book['barcode'])
                 if(data.length > 0){
                     for(var n = 0; n < data.length; n++) {
                         if(data[n].id == response[0]['id']) {
+                            alert('exist')
                             console.log('true')
                             data[n].quantity = $('input[name=quantity]').val();
-                            console.log('0')
-                            $("#tbodyMB").empty();
-                            for (var i = 0; i < data.length; i++) {
-                                var total = data[i].quantity * data[i].rate
-                                var row = $("<tr />")
-                                $("#invoiceTable").append(row);
-                                row.append($("<td>" + data[i].name + "</td>"));
-                                row.append($("<td>" + data[i].quantity + "</td>"));
-                                row.append($("<td>" + data[i].rate + "</td>"));
-                                row.append($("<td>" + total + "</td>"));
-                                row.append($("<td>" + '<button id="btnDeleteMBk" type="button" class="btn btn-outline-secondary" value=' + data[i].id + ' deleterow"><i class="icofont-ui-delete text-danger"></i></button>' + "</td>"));
-                            }
+                            viewTable()
                         }
                         else{
-                            obj = {
-                                'id': response[0]['id'],
-                                'name': response[0].book['name'],
-                                'quantity': $('input[name=quantity]').val(),
-                                'rate' : response[0].book['sales_rate']
-                            }
-                            data.push(obj)
-                            console.log(data)
-                            $("#tbodyMB").empty();
-                            for (var i = 0; i < data.length; i++) {
-                                var total = data[i].quantity * data[i].rate
-                                var row = $("<tr />")
-                                $("#invoiceTable").append(row);
-                                row.append($("<td>" + data[i].name + "</td>"));
-                                row.append($("<td>" + data[i].quantity + "</td>"));
-                                row.append($("<td>" + data[i].rate + "</td>"));
-                                row.append($("<td>" + total + "</td>"));
-                                row.append($("<td>" + '<button id="btnDeleteMBk" type="button" class="btn btn-outline-secondary" value=' + data[i].id + ' deleterow"><i class="icofont-ui-delete text-danger"></i></button>' + "</td>"));
-                            }
+                             addData(response[0]['id'],response[0].book['name'],$('input[name=quantity]').val())
                         }
                     }
                 }
                 else{
+                    alert('Not exist')
                     obj = {
                         'id': response[0]['id'],
                         'name': response[0].book['name'],
-                        'quantity': '1',
+                        'quantity': $('input[name=quantity]').val(),
                         'rate' : response[0].book['sales_rate']
                     }
                     data.push(obj)
                     console.log(data)
-                    $("#tbodyMB").empty();
-                    for (var i = 0; i < data.length; i++) {
-                        var total = data[i].quantity * data[i].rate
-                        var row = $("<tr />")
-                        $("#invoiceTable").append(row);
-                        row.append($("<td>" + data[i].name + "</td>"));
-                        row.append($("<td>" + data[i].quantity + "</td>"));
-                        row.append($("<td>" + data[i].rate + "</td>"));
-                        row.append($("<td>" + total + "</td>"));
-                        row.append($("<td>" + '<button id="btnDeleteMBk" type="button" class="btn btn-outline-secondary" value=' + data[i].id + ' deleterow"><i class="icofont-ui-delete text-danger"></i></button>' + "</td>"));
-                    }
+                    viewTable();                            
                 }
                
             }
@@ -84,6 +48,18 @@ $("#invoiceForm").submit(function(){
     });
     return false
 });
+
+function addData(id,name,quantity,rate){
+    obj = {
+        'id': id,
+        'name': name,
+        'quantity': quantity,
+        'rate' : rate,
+    }
+    data.push(obj)
+    console.log(data)
+    viewTable();    
+}
 
 function viewTable (){
     $("#tbodyMB").empty();
